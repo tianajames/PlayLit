d0fdeef83c21f588556695cfb245ab0671f5b33a
Tiana <tianajames1212@gmail.com>
2019-07-30 12:41:26 -0400

    tiana

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class ArtistPage(webapp2.RequestHandler): #Page that receives a post request with the value of moodchoose.
-    def post(self):
-        results_template = the_jinja_env.get_template('artistpage.html') #Selects an html file to be used as template
-        mood_choice = self.request.get('moodchoose') #THE ACTUAL MAGIC
-        the_variable_dict = {"mood":mood_choice}
-        the_variable_dict = [""]
-        self.response.write(results_template.render(the_variable_dict))
-
 class MoodPage(webapp2.RequestHandler): #Initial page
     def get(self):
         print ("I am here")
@@ -41,15 +33,6 @@ class MainPage(webapp2.RequestHandler):
         welcome_template = the_jinja_env.get_template('main.html')
         self.response.write(welcome_template.render())
 
-class ArtistPage(webapp2.RequestHandler):
-    def get(self): #for a get request
-        welcome_template = the_jinja_env.get_template('artistpage.html')
-        self.response.write(welcome_template.render())
-
-    def post(self):
-        welcome_template = the_jinja_env.get_template('artistpage.html')
-        self.response.write(welcome_template.render())
-
 class FinalPage(webapp2.RequestHandler):
     def get(self): #for a get request
         welcome_template = the_jinja_env.get_template('final.html')
@@ -57,7 +40,6 @@ class FinalPage(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/mood', MoodPage),
-    ('/artist', ArtistPage),
     ('/welcome', WelcomePage),
     ('/main', MainPage),
     ('final', FinalPage)
