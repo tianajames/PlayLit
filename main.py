b89591522b760697fb1fdbd8e0f35358a0a96717
Haileigh <haileigh8801@gmail.com>
2019-07-30 11:09:52 -0400

    hail

--- main.py
+++ main.py
@@ -12,17 +12,20 @@ class ArtistPage(webapp2.RequestHandler): #Page that receives a post request wit
         results_template = the_jinja_env.get_template('artistpage.html') #Selects an html file to be used as template
         mood_choice = self.request.get('moodchoose') #THE ACTUAL MAGIC
         the_variable_dict = {"mood":mood_choice}
+        the_variable_dict = [""]
         self.response.write(results_template.render(the_variable_dict))
 
 class MoodPage(webapp2.RequestHandler): #Initial page
     def get(self):
+        print ("I am here")
         welcome_template = the_jinja_env.get_template('moodpage.html')
 
-        self.response.write(welcome_template.render())
 
     def post(self):
         welcome_template = the_jinja_env.get_template('moodpage.html')
-        self.response.write(welcome_template.render())
+        occasion_choice = self.request.get('Occasion')
+        the_variable_dict = {"occasion":occasion_choice}
+        self.response.write(welcome_template.render(the_variable_dict))
 
 class WelcomePage(webapp2.RequestHandler):
     def get(self): #for a get request
